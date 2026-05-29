da1010bd1ad3eb66440404a40a011235f633df8f
Uttkarsh singh <singhuttkarsh450@gmail.com>
2026-05-29 10:16:48 +0530

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        for(auto &p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+
+        // vector<bool> visited(numCourses, false)
+
+        vector<int> indegree(numCourses,0);
+
+        for(int i=0;i<numCourses;i++){
+            for(int &v: adj[i]){
+                indegree[v]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            ans.push_back(u);
+
+            for(int &v: adj[u]){
+                if(--indegree[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+
+        return (ans.size() == numCourses);
+    }
+};
